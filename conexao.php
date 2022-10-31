f50dcdaa6c86f7401b0e917ef01e487f6799a6da
EmilynnZabala <101806716+EmilynnZabala@users.noreply.github.com>
2022-10-31 08:29:35 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+    $servidor = "Localhost";
+    $usuario = "empregos";
+    $senha = "12345678";
+    $banco = "empregosAA";
+
+    $cn = new PDO("mysql:host=$servidor;dbname=$banco",$usuario,$senha);
+?>
---
+++ pesquisarr.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php
+    include 'conexao.php';
+    $Busca_cargo=$cn->query("select Cargo from tbl_Empregos group by Cargo");
+    $Busca_area=$cn->query("select Area from tbl_Empregos group by Area");
+    ?>
+
+</head>
+
+
+<body>
+<form name="frmbusca" method="get" action="pesquisarr.php">
+
+<select name="SltCargo">
+            <option value = "Selecione">Selecione</opition>
+            <?php 
+            while($exibeCargo=$Busca_cargo->fetch(PDO::FETCH_ASSOC)){?>
+            <option values = <?php echo $exibeCargo['Cargo'];?>>
+            <?php echo $exibeCargo['Cargo']; ?></option>
+            <?php } ?>
+</select>
+
+
+<select name="SltArea">
+            <option value = "Selecione">Selecione</opition>
+            <?php 
+            while($exibeArea=$Busca_area->fetch(PDO::FETCH_ASSOC)){?>
+            <option values = <?php echo $exibeArea['Area'];?>>
+            <?php echo $exibeArea['Area']; ?></option>
+            <?php } ?>
+</select>
+</form>
+
+<div>
+    <?php
+    if (!isset($_GET['SltCargo']) and !isset($_GET['SltArea']))
+    {
+        
+    } 
+    else {
+        echo "<table border ="1" width="80%"><tr>";
+    }
+</body>
+</html> 
